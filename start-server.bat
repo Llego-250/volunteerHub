b35fc57541df2593ac63c6ef4fb3a5a785e46431
Llego-250 <solideaze1251@gmail.com>
2026-02-23 19:46:29 +0200

    the bat of the system

--- start-server.bat
+++ start-server.bat
@@ -1,2 +1,17 @@
 @echo off
-php -S localhost:8000
+echo.
+echo ============================================
+echo  VolunteerHub Server Setup
+echo ============================================
+echo.
+echo PHP is not installed or not in PATH.
+echo.
+echo Please install XAMPP from:
+echo https://www.apachefriends.org/download.html
+echo.
+echo After installing XAMPP:
+echo 1. Copy this project to: C:\xampp\htdocs\volunteerHub
+echo 2. Start Apache and PostgreSQL from XAMPP Control Panel
+echo 3. Access at: http://localhost/volunteerHub
+echo.
+pause
