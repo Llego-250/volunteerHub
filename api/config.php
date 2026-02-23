85e670c1593265057582423a4707dc0b529cf30f
Llego-250 <solideaze1251@gmail.com>
2026-02-23 12:21:58 +0200

    Remove the OPTIONS exit from config.php so individual endpoint files can handle it

--- api/config.php
+++ api/config.php
@@ -21,8 +21,4 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json');
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit(0);
-}
 ?>
