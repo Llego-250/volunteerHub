2f310bc5941a7e898686182820d41f5c8bc8a738
Llego-250 <solideaze1251@gmail.com>
2026-02-25 13:16:37 +0200

    access contror

--- api/contact.php
+++ api/contact.php
@@ -1,4 +1,13 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit(0);
+}
+
 require_once 'config.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
